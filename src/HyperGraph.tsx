15093ad6664d0ead3f3b8af4f0e359be87e1faac
eytan.s <eytan.singher@gmail.com>
2019-10-10 11:06:39 +0300

    Different colors for edges and nodes

--- src/HyperGraph.tsx
+++ src/HyperGraph.tsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Graph from 'vis-react'
 
 interface VisNode {
     id: number;
     label: string;
     shape?: string;
+    color?: string;
 }
 
 interface VisEdge {
@@ -33,15 +34,22 @@ export class Node {
     id: ID;
     literal?: string;
     shape?: string;
+    color: string;
 
-    constructor(id: ID, literal?: string, shape = "ellipse") {
+    constructor(id: ID, literal?: string, shape = "ellipse", color = "#80b3ff") {
         this.id = id;
         this.literal = literal;
         this.shape = shape;
+        this.color = color
     }
 
     toVisNode(): VisNode {
-        return {id: this.id.id, shape: this.shape, label: this.literal !== undefined ? this.literal : this.id.toString()}
+        return {
+            id: this.id.id,
+            shape: this.shape,
+            label: this.literal !== undefined ? this.literal : this.id.toString(),
+            color: this.color
+        }
     }
 }
 
@@ -60,7 +68,7 @@ export class HyperEdge {
 interface HyperGraphProps {
     nodes: Node[];
     edges: HyperEdge[];
-    options?: {hasTarget: boolean};
+    options?: { hasTarget: boolean };
 }
 
 const optionsDefault = {
@@ -81,8 +89,8 @@ export class HyperGraph extends Component<HyperGraphProps, any> {
     private readonly hasTarget: boolean;
 
     private events = {
-        select: function(event: any) {
-            let { nodes, edges } = event;
+        select: function (event: any) {
+            let {nodes, edges} = event;
         }
     };
 
@@ -109,23 +117,23 @@ export class HyperGraph extends Component<HyperGraphProps, any> {
 
     render() {
         console.log("rendering hyper graph");
-        let maxId = Math.max(...this.props.nodes.map((value: Node) => { return value.id.id; }));
+        let maxId = Math.max(...this.props.nodes.map((value: Node) => {
+            return value.id.id;
+        }));
 
         let nodes: VisNode[] = this.props.nodes.map((value, index, array) => value.toVisNode());
         let edges: VisEdge[] = [];
 
         for (let edge of this.props.edges) {
             maxId += 1;
-            let newNode = new Node(new ID(maxId), edge.type, "box");
+            let newNode = new Node(new ID(maxId), edge.type, "box", "#88cc00");
             nodes.push(newNode.toVisNode());
             edges = edges.concat(edge.sources.map((value: ID, index: number) => {
                 if (this.hasTarget && edge.hasTarget && index === 0) {
                     return {to: value.id, from: newNode.id.id}
-                }
-                else if (!this.hasTarget || !edge.hasTarget) {
+                } else if (!this.hasTarget || !edge.hasTarget) {
                     return {from: value.id, to: newNode.id.id, label: "arg " + (index + 1)}
-                }
-                else return {from: value.id, to: newNode.id.id, label: "arg " + index}
+                } else return {from: value.id, to: newNode.id.id, label: "arg " + index}
             }));
         }
 
@@ -136,7 +144,7 @@ export class HyperGraph extends Component<HyperGraphProps, any> {
 
         console.log("returning new hyper graph");
         return (
-            <Graph graph={graph} options={this.options}  events={this.events}/>
+            <Graph graph={graph} options={this.options} events={this.events}/>
         );
     }
 }
